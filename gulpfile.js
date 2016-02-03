7497877dc14b727219c6b5d3d4640a35285287bf
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-03 01:52:03 +0000

    Added Working api call and rending with nodes

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,56 @@
 var gulp = require('gulp');
 var debug = require('gulp-debug');
 var flatten = require('gulp-flatten');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
-gulp.task('third-party-scripts', function(){
-	return gulp.src(['node_modules/**/knockout-latest.js','node_modules/**/jquery.min.js'])
-	.pipe(debug())
-	.pipe(flatten())
-	.pipe(gulp.dest('./public/scripts'));
+gulp.task('third-party-scripts', function () {
+    return gulp.src(['node_modules/**/jquery.min.js', 'node_modules/**/material.js', 'node_modules/**/bootstrap.js', 'node_modules/**/ripples.js'])
+        .pipe(debug())
+        .pipe(flatten())
+        .pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('scripts', ['third-party-scripts'], function(){
-	return gulp.src('src/scripts/**js')
-	.pipe(gulp.dest('./public/scripts'));
-})
 
-gulp.task('style', function(){	
-	return gulp.src('src/styles/*.css')
-	.pipe(gulp.dest('./public/stylesheets'));
+gulp.task('third-party-less', function () {
+    gulp.src(['node_modules/robin-less/less/**/*'])
+        .pipe(gulp.dest('./src/styles/less'));
+
+    gulp.src(['node_modules/bootstrap-material-design/dist/css/ripples.min.css'])
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('default', ['style', 'scripts']);
+
+gulp.task('scripts', ['third-party-scripts'], function () {
+    return gulp.src('src/scripts/*.js')
+        .pipe(gulp.dest('./public/js'));
+});
+
+
+gulp.task('less', ['third-party-less', ], function () {
+    gulp.src('./src/styles/less/style.less') // path to your file
+        .pipe(less())
+        .pipe(gulp.dest('./public/css/'));
+
+});
+
+
+
+gulp.task('img', function () {
+    return gulp.src('./src/img/*').pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{
+            removeViewBox: false
+        }],
+        use: [pngquant()]
+    })).pipe(gulp.dest('./public/img/'));
+});
+
+
+
+
+
+gulp.task('default', ['scripts', 'less', 'img']);
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ console.log("Setup Complete");
 var express = require('express'),
     stylus = require('stylus'),
     nib = require('nib'),
-    request = require('request')
-
+    request = require('request');
 
 var app = express();
 app.use(express.json()); // to support JSON-encoded bodies
@@ -27,73 +26,40 @@ app.use(stylus.middleware({
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Home'
-    })
-})
-
-app.get('/nightsout', function (req, res) {
-    res.render('nightsout', {
-        title: 'Home'
-    })
-})
-
-app.get('/idea', function (req, res) {
-    res.render('idea', {
-        title: 'Home'
-    })
-})
-
-app.get('/companylayout', function (req, res) {
-    res.render('companyLayout', {
-        title: 'Home'
-    })
-})
+    var url = "https://account.xbox.com/en-us/gameclips/loadByUser?gamerTag=";
+    var gamerTag = "";
 
 
-app.get('/swag', function (req, res) {
-    res.render('swag', {
-        title: 'Goodies'
-    })
-})
+    req.query['gamerTag'] ? gamerTag = req.query['gamerTag'] : gamerTag = "E";
 
-app.get('/manageHolidays', function (req, res) {
-    res.render('holidayForm', {
-        title: 'Holiday Request'
-    })
-})
-
-app.get('/test', function (req, res) {
-    res.render('test', {
-        title: 'batman'
-    })
-})
-
-app.post('/test', function (req, res) {
-    app.username = req.body.username;
-    app.password = req.body.password;
-})
-
-
-app.get('/internship', function (req, res) {
-    res.render('internship', {
-        title: 'batman'
-    })
-})
-
-
-app.get('/userDetails', function (req, res) {
-    console.log(app);
-    var url = 'https://' + app.username + ':' + app.password + '@digitalcrew.teamwork.com/me.json';
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var response = JSON.parse(body);
-            app.account = response
-
-        } else {
-            console.log("Got an error: ", error, ", status code: ", response.statusCode);
-        }
+    console.log(gamerTag);
+    request(url + gamerTag, function (err, response, body) {
+        var dataGram = JSON.parse(body);
+        res.render('index', {
+            title: 'Home',
+            data: dataGram
+        })
     });
+
+
+
+
 })
+
+app.get('/about', function (req, res) {
+    res.render('about', {
+        title: 'about'
+    })
+})
+
+
+app.get('/contact', function (req, res) {
+    res.render('contact', {
+        title: 'contact'
+    })
+})
+
+
+
 
 app.listen(8081, '0.0.0.0')
